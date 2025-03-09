f58fdaed25df839b6d358da324220b64781d5ca7
Naveen <naveendanushka293@gmail.com>
2025-03-09 07:20:34 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "LAKA-MD=F8IBDRpZ#Com6Gq1iaT10f3KL0Cs-rsUDG0sWIkDqnQ4-ceVQkYE",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=tshx0CDK#8FtgGSMjI8GfwOQs2Viao-wVUBInznlTHMvsjn-S5bA",
 PREFIX: process.env.PREFIX || '.' ,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "true" : process.env.AUTO_VOICE,
